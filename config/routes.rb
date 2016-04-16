2a8cdcf93796559c0514bd8dcc484cc00f511c5b
michaelkthai <mthai27@gmail.com>
2016-04-16 14:56:37 -0400

    Debug by repositioning the sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,9 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show]
   resources :users, only: [:new, :create] do
     resources :reservations, only: [:index, :new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
   end
-
-  # get 'restaurants/index'
-  # get 'restaurants/show'
-
-  # get 'users/new'
-  # get 'users/create'
-
-  # get 'reservations/new'
-  # get 'reservations/create'
+  resources :sessions, only: [:new, :create, :destroy]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
