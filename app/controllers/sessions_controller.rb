49df6c2c1bd75415afe459962080cc3d83b5fb91
Giang La <peter.la@peterla.com>
2016-04-17 13:42:55 -0400

    Added links to signup, login, logout and flash messages to display login status.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to products_url, notice: "Logged out."
+    redirect_to restaurants_url, notice: "Logged out."
   end
 end
