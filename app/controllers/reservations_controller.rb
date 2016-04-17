066bc0174d7f8f526882ca24b0815bfa5a6d9405
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-17 14:55:03 -0400

    Created reservation view and logic

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,29 @@
 class ReservationsController < ApplicationController
+  before_action :load_restaurant
+  before_action :ensure_logged_in, only: [:create, :destroy]
+
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      redirect_to restaurants_url, notice: "reservation created successfully"
+    else
+      render "restaurants/show"
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :restaurant_id, :date, :time, :party_size)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
