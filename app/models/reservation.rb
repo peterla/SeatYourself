2499611a1ef6ee09ca4b040ce4e0756f02ea2378
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-17 17:03:57 -0400

    Added validation to reservation to check if reservation is under restaurant capacity

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,17 @@ class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
 
+  validate :less_than_max_capacity
+
+  protected
+
+  def less_than_max_capacity
+    other_people = Reservation.where(date: self.date, time: self.time).sum(:party_size)
+    binding.pry
+    if other_people + self.party_size > self.restaurant.capacity
+
+      errors.add(:base, "Sorry, too many people!")
+    end
+  end
+
 end
