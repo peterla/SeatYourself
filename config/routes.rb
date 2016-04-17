0f107e9c035cb829710ca63f3cb180d273704c7c
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-04-17 13:53:56 -0400

    changed reservation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   root 'restaurants#index'
-  resources :restaurants, only: [:index, :show]
-  resources :users, only: [:new, :create] do
+  resources :restaurants, only: [:index, :show] do
     resources :reservations, only: [:index, :new, :create]
   end
-  resources :sessions, only: [:new, :create, :destroy]
   
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
