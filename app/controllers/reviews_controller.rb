4bad4bcdf4d62212e9d7a68c59a6c70095e3c0d9
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-18 13:00:20 -0400

    Updated review sort order desc on restaurant show page and redirect back to restaurant show when review is created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,7 @@ class ReviewsController < ApplicationController
     @review.user = current_user
 
     if @review.save
-      redirect_to restaurants_url, notice: "Review created successfully"
+      redirect_to restaurant_path(params[:restaurant_id]), notice: "Review created successfully"
     else
       render "restaurants/show"
     end
