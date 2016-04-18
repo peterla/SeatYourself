ad15911ca76cadf412bae28e68fbb9f90f495c34
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-17 23:17:04 -0400

    Created review model, controller and added review functionality to restaurants show page.  Also removed binding in reservation.rb.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,8 @@ class Restaurant < ActiveRecord::Base
 
   has_many :reservations
   has_many :users, through: :reservations
-  belongs_to  :cuisine
+  belongs_to :cuisine
+  has_many :reviews
+  has_many :users, through: :reviews
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414212919) do
+ActiveRecord::Schema.define(version: 20160418025526) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string   "cuisine_type"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20160414212919) do
     t.integer  "capacity"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
