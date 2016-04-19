34a4d312cd835471d72d208c07b8d1b11559b589
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-04-19 07:36:08 -0400

    have the basic email functionality working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
+        UserMailer.welcome_email(@user).deliver_now 
         redirect_to restaurants_url, notice: "Thank you for Signing up"
       else
         render "new"
