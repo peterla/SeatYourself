f324619eead4ba2cee945cf93b9a5294284e06c6
Giang La <peter.la@peterla.com>
2016-04-18 20:57:38 -0400

    Added a Reservation test, added a factory method to restaurants.rb

--- test/factories/restaurants.rb
+++ test/factories/restaurants.rb
@@ -6,4 +6,12 @@ FactoryGirl.define do
     phone_number "1234567"
     capacity 25
   end
+
+  factory :full_restaurant, class: 'Restaurant' do
+    name "Bymark"
+    cuisine_id 1
+    address "28 Wellington St."
+    phone_number "2882828"
+    capacity 5
+  end
 end
